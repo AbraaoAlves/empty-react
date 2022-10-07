f1aceb1db29ec46aaf3775f0ed9fa33dd0e504e4
AbraaoAlves <abraao.teodosio@gmail.com>
2022-10-07 10:34:49 -0300

    chore(index): add prop

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Relogio() {
-
+function Relogio(props) {
+  const date = props.date;
   return <div>
     <h1>Hello, world!</h1>
-    <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    <h2>It is {date}.</h2>
   </div>
 } 
 
-root.render(<Relogio />);
+root.render(<Relogio date={new Date().toLocaleTimeString()} />);
