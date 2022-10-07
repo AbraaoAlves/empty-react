13b45c6dbedb369b1105994268172ebf73df4912
AbraaoAlves <abraao.teodosio@gmail.com>
2022-10-07 11:18:02 -0300

    chore(index): add update button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';  
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Relogio(props) {
-  const date = props.date;
-  return <div>
-    <h1>Hello, world!</h1>
-    <h2>It is {date}.</h2>
-  </div>
-} 
+function Relogio() {
+  const [state, setState] = useState(new Date().toLocaleTimeString());
 
-root.render(<Relogio date={new Date().toLocaleTimeString()} />);
+  return (
+    <div>
+      <h2>It is {state}.</h2>
+
+      <button onClick={() => setState(new Date().toLocaleTimeString())}>
+        Atualizar
+      </button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <Relogio />
+      <Relogio />
+      <Relogio />
+    </>
+  );
+}
+
+root.render(<App />);
