1cf2cb8923fb3b467c32a7a37ccf29816e96d7df
AbraaoAlves <abraao.teodosio@gmail.com>
2022-10-07 11:20:40 -0300

    docs(index): add comment todo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 function Relogio() {
   const [state, setState] = useState(new Date().toLocaleTimeString());
 
+
+  // atualizar o state a cada segundo
+  // setInterval(() => {
+  //   setState(new Date().toLocaleTimeString());
+  // }, 1000)
+
+  // a atualizacao deve parar quando o component sair da tela
+
+
   return (
     <div>
       <h2>It is {state}.</h2>
